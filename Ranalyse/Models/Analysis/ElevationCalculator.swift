2a923884a485ae6f8977ddcf45a1b89e6b3ebd85
Mykhailo Moiseienko <mykhailo.moiseienko@vivint.com>
2019-05-12 23:41:53 +0300

    Implement calculation of Cumulative Elevation Gain

--- Ranalyse/Models/Analysis/ElevationCalculator.swift
+++ Ranalyse/Models/Analysis/ElevationCalculator.swift
@@ -10,5 +10,31 @@ import Foundation
 import CoreGPX
 
 class ElevationCalculator {
+    func cumulativeElevationGain(forGPX gpx: GPXRoot) -> Double {
+        return gpx.tracks
+            .map { cumulativeElevationGain(forTrack: $0) }
+            .reduce(0.0, +)
+    }
     
+    func cumulativeElevationGain(forTrack track: GPXTrack) -> Double {
+        return track.tracksegments
+            .map { cumulativeElevationGain(forTrackSegment: $0) }
+            .reduce(0.0, +)
+    }
+    
+    func cumulativeElevationGain(forTrackSegment trackSegment: GPXTrackSegment) -> Double {
+        var cumulativeElevationGain = 0.0
+        for i in 1..<trackSegment.trackpoints.count {
+            let point1 = trackSegment.trackpoints[i - 1]
+            let point2 = trackSegment.trackpoints[i]
+            
+            let difference = elevationDifferenceBetweenPoints(point1: point1, point2: point2)
+            cumulativeElevationGain += difference > 0 ? difference : 0
+        }
+        return cumulativeElevationGain
+    }
+    
+    func elevationDifferenceBetweenPoints(point1: GPXTrackPoint, point2: GPXTrackPoint) -> Double {
+        return (point2.elevation ?? 0) - (point1.elevation ?? 0)
+    }
 }
